5863385b8732936c33de1f2ffb85f55143d7b24b
woshiyxz00 <youxingzhi@qq.com>
2016-05-10 19:17:31 +0800

    丑陋的微信demo结束

--- app.js
+++ app.js
@@ -25,9 +25,9 @@ var tpl = heredoc(function() {/*
     </head>
     <body>
         <h1 style="margin-top:200px;font-size:5rem;">开始录音翻译，查询你喜欢的电影</h1>
+         <h1 id="search">查询：</h1>
         <p id="title"></p>
          <div id="director"></div>
-        <h1 id="search">查询：</h1>
          <div id="year"></div>
         <div id="poster"></div>
         <script src="http://zeptojs.com/zepto-docs.min.js"></script>
@@ -41,6 +41,12 @@ var tpl = heredoc(function() {/*
                  nonceStr: '<%=noncestr %>', // 必填，生成签名的随机串
                  signature: '<%=signature %>',// 必填，签名，见附录1
                  jsApiList: [
+                     'previewImage',
+                     'onMenuShareTimeline',
+                     'onMenuShareAppMessage',
+                     'onMenuShareQQ',
+                     'onMenuShareWeibo',
+                     'onMenuShareQZone',
                      'startRecord',
                      'stopRecord',
                      'onVoiceRecordEnd',
@@ -49,6 +55,21 @@ var tpl = heredoc(function() {/*
              });
 
              wx.ready(function() {
+                // 分享给好友
+                var shareContent = {
+                     title: '默认标题', // 分享标题
+                     desc: '我搜出来了个鬼', // 分享描述
+                     link: 'https://www.baidu.com', // 分享链接
+                     imgUrl: '#', // 分享图标
+                     success: function () {
+                        window.alert('分享成功');
+                     },
+                     cancel: function () {
+                        window.alert('分享失败');
+                     }
+                }
+                 wx.onMenuShareAppMessage(shareContent);
+
                  wx.checkJsApi({
                      jsApiList: ['onVoiceRecordEnd'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
                      success: function(res) {
@@ -57,6 +78,11 @@ var tpl = heredoc(function() {/*
                      // 以键值对的形式返回，可用的api值true，不可用为false
                      // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
                      }
+                 });
+                 var slides = null;
+                 // 点击poster
+                 $('#poster').on('tap', function(){
+                    wx.previewImage(slides);
                  });
                  // 点击标题
                  var isRecording = false;
@@ -90,13 +116,37 @@ var tpl = heredoc(function() {/*
                                         success: function(data) {
                                             //alert(data)
                                             var subject = data.subjects[0];
+                                            $('#title').html(subject.title);
                                             $('#director').html(subject.directors[0].name);
                                             $('#poster').html('<img src="'+subject.images.large+'"/>');
                                             $('#year').html(subject.year);
+                                            alert('点击海报查看更多哦');
+
+                                            shareContent = {
+                                                 title: subject.title, // 分享标题
+                                                 desc: '我搜出来了'+subject.title, // 分享描述
+                                                 link: 'http://5xh1glu64e.proxy.qqbrowser.cc/movie', // 分享链接
+                                                 imgUrl: subject.images.small, // 分享图标
+                                                 type: 'link', // 分享类型,music、video或link，不填默认为link
+                                                 dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+                                                 success: function () {
+                                                    window.alert('分享成功');
+                                                 },
+                                                 cancel: function () {
+                                                    window.alert('分享失败');
+                                                 }
+                                             }
+                                            wx.onMenuShareAppMessage(shareContent);
+
+                                             slides = {
+                                                current: subject.images.large,
+                                                urls:[subject.images.large]
+                                             };
+                                             data.subjects.forEach(function(item){
+                                                slides.urls.push(item.images.large);
+                                             });
                                         },
                                         error: function(err){
-                                            alert('err:');
-                                            (err);
                                         }
                                     });
                                     //alert(res.translateResult); // 语音识别的结果
@@ -164,7 +214,9 @@ app.use(function *(next) {
        var wechatApi = new Wechat(config.wechat);
        var data = yield wechatApi.fetchAccessToken();
        var access_token = data.access_token;
+
        var ticketData = yield wechatApi.fetchTicket(access_token);
+       console.log(ticketData);
        var ticket = ticketData.ticket;
        //使用qq的调试工具时会加端口号，这是一个坑
        var url = this.href.replace(':8000','');
